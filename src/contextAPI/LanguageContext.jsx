25538e8bd2b9f0a6b3308cef6b59b683b3757b8c
Alican Tarım <alicantarim@hotmail.com>
2025-02-04 14:34:24 +0300

    Add: ContextAPI folder

---
+++ src/contextAPI/LanguageContext.jsx
@@ -0,0 +1,31 @@
+import dataJSON from "../data.json";
+import { createContext, useContext, useEffect, useState } from "react";
+
+const getBrowserLanguage = () => {
+  const browserLanguage = navigator.language.slice(0, 2);
+  return browserLanguage === "tr" || browserLanguage === "en"
+    ? browserLanguage
+    : "en";
+};
+
+const LanguageContext = createContext();
+
+export const LanguageProvider = ({ children }) => {
+  const [language, setLanguage] = useState(() => {
+    return localStorage.getItem("language") || getBrowserLanguage();
+  });
+
+  useEffect(() => {
+    localStorage.setItem("language", language);
+  }, [language]);
+
+  const localizedData = dataJSON[language];
+
+  return (
+    <LanguageContext.Provider value={{ setLanguage, language, localizedData }}>
+      {children}
+    </LanguageContext.Provider>
+  );
+};
+
+export const useLanguage = () => useContext(LanguageContext);
